d7b94694b8f273f2d113327f382128f41d25b8b0
SpenserCai <spensercai@gmail.com>
2023-08-06 20:56:55 +0800

    fix ui

--- scripts/ui_deoldify.py
+++ scripts/ui_deoldify.py
@@ -3,7 +3,7 @@ Author: SpenserCai
 Date: 2023-08-06 20:15:12
 version: 
 LastEditors: SpenserCai
-LastEditTime: 2023-08-06 20:48:23
+LastEditTime: 2023-08-06 20:56:26
 Description: file content
 '''
 from modules import script_callbacks, paths_internal
@@ -19,7 +19,7 @@ def deoldify_tab():
         with gr.Tab("Video"):
             with gr.Row():
                 with gr.Column():
-                    video_input = gr.inputs.Video(label="原视频",type="file")
+                    video_input = gr.inputs.Video(label="原视频")
                     # 一个名为render_factor的滑块，范围是1-50，初始值是35，步长是1
                     render_factor = gr.Slider(minimum=1, maximum=50, step=1, label="Render Factor")
                     render_factor.value = 35
@@ -27,7 +27,7 @@ def deoldify_tab():
                     artistic = gr.Checkbox(label="Rrtistic")
                     artistic.value = False
                 with gr.Column():
-                    video_output = gr.outputs.Video(label="修复后的视频",type="auto")
+                    video_output = gr.outputs.Video(label="修复后的视频")
             run_button = gr.Button(label="Run")
             run_button.click(inputs=[video_input,render_factor,artistic],outputs=[video_output],fn=process_image)
 
